d58381b2e3fbc05b7caa790c23606dffb32a53f2
SakoGuru <bltpn6@mail.missouri.edu>
2015-04-14 13:32:28 -0500

    Replaced default ViewController (VC)

---
+++ NewsLater/NewsLaterFeedController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsLaterFeedController.swift
+//  NewsLater
+//
+//  Created by University of Missouri on 4/14/15.
+//  Copyright (c) 2015 Matt Zuzolo. All rights reserved.
+//
+
+import UIKit
+
+class NewsLaterFeedController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
