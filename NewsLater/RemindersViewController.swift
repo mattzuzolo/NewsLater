1278767dd9dd1da02b7dd78c7605a3abbff4f570
mpshos <Mike@mwc-223141.dhcp.missouri.edu>
2015-04-16 19:24:20 -0500

    added article view and reminders view
    added article view and reminders view

---
+++ NewsLater/RemindersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RemindersViewController.swift
+//  NewsLater
+//
+//  Created by Mike on 4/16/15.
+//  Copyright (c) 2015 Matt Zuzolo. All rights reserved.
+//
+
+import UIKit
+
+class RemindersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
