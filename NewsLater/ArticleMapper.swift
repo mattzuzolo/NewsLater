2d1e334db4307576be39f7db975469a6e5ae2f4b
SakoGuru <bltpn6@mail.missouri.edu>
2015-04-21 13:55:52 -0500

    Added the error function to one of the view controllers as well as a test call to the NYT api (not done yet).

--- NewsLater/ArticleMapper.swift
+++ NewsLater/ArticleMapper.swift
@@ -16,9 +16,33 @@ class ArticleMapper: NSObject{
     
     let NYTUrl = "https://api.nytimes.com/svc/topstories/v1/home.json?api-key=3caa4c3969858fadeaa4bbe5a3529235:13:71572887"
     
-    func loadArticles(){
-        
-        
+    func loadArticles(completionHandler: (ArticleMapper, String?) -> Void){
+        callAPI(NYTUrl, completionHandler: completionHandler)
+    }
+    
+    func callAPI(URL: String, completionHandler: (ArticleMapper, String?) -> Void){
+        if let url = NSURL(string: URL) {
+            let urlRequest = NSMutableURLRequest(URL: url)
+            let session = NSURLSession.sharedSession()
+            let jsonParseTask = session.dataTaskWithRequest(urlRequest, completionHandler: {
+                (data, response, error) -> Void in
+                if error != nil {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        completionHandler(self, error.localizedDescription)
+                    })
+                } else {
+                    //self.jsonParser(data, completionHandler)
+                    let jsonData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    let articles = Mapper<Article>().map(jsonData)
+                }
+            })
+            
+            jsonParseTask.resume()
+        } else {
+            dispatch_async(dispatch_get_main_queue(), {
+                completionHandler(self, "Invalid URL")
+            })
+        }
         
     }
     //let article = Mapper<Article>().map(JSONString)
