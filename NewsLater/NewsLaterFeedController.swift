abb4bfcf78e41f9c1e20df0b2c293d345d6892e1
mpshos <Mike@mwc-178175.dhcp.missouri.edu>
2015-05-09 14:20:56 -0500

    moved cell configuration to custom cell class, added custom cells to recently read

--- NewsLater/NewsLaterFeedController.swift
+++ NewsLater/NewsLaterFeedController.swift
@@ -14,10 +14,15 @@ class NewsLaterFeedController: UIViewController, UITableViewDataSource, UITableV
     var selectedArticle: Article?
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
+    let dateFormatter = NSDateFormatter()
+    
     @IBOutlet weak var feedView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set up the date formatter
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         
         //set row height so that 5 stories will fill feed
         feedView.rowHeight = feedView.frame.height / 5
@@ -74,8 +79,8 @@ class NewsLaterFeedController: UIViewController, UITableViewDataSource, UITableV
         
         let cell = tableView.dequeueReusableCellWithIdentifier("article_cell", forIndexPath: indexPath) as! ArticleTableViewCell
         cell.title?.text = articleMapper.filteredArticles[indexPath.row].headline!
-        cell.subtitle?.text = articleMapper.filteredArticles[indexPath.row].publishedDate?.description
-        configureCell(cell)
+        cell.subtitle?.text = "\(articleMapper.filteredArticles[indexPath.row].publication!) / \(articleMapper.filteredArticles[indexPath.row].publishedDate?.description)"
+        cell.configureCell(feedView.rowHeight, frameWidth: feedView.frame.width)
         return cell
     }
     
@@ -99,34 +104,4 @@ class NewsLaterFeedController: UIViewController, UITableViewDataSource, UITableV
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    func configureCell(cell: ArticleTableViewCell){
-        let width = feedView.frame.width
-        let height = feedView.rowHeight
-        
-        let views = ["thumbnail": cell.thumbnail, "title": cell.title, "subtitle": cell.subtitle]
-        
-        //Visual Format Language representation of needed constraints
-        let imageHStr = "H:|-\(width / 10)-[thumbnail(<=\(height * 8 / 10))]-(>=\(width - ((8 * height / 10) + width / 10)))-|"
-        let imageVStr = "V:|-\(height / 10)-[thumbnail(<=\(height * 8 / 10))]-(>=\(height / 10))-|"
-        let titleHStr = "H:|-\((2 * width / 10) + (height * 8 / 10))-[title]-|"
-        let subtitleHStr = "H:|-\((2 * width / 10) + (height * 8 / 10))-[subtitle]-|"
-        let labelVStr = "V:|-\(height / 10)-[title][subtitle]-\(height / 10)-|"
-        
-        cell.thumbnail.setTranslatesAutoresizingMaskIntoConstraints(false)
-        cell.title.setTranslatesAutoresizingMaskIntoConstraints(false)
-        cell.subtitle.setTranslatesAutoresizingMaskIntoConstraints(false)
-        
-
-        var thumbnailConstrH = NSLayoutConstraint.constraintsWithVisualFormat(imageHStr, options: NSLayoutFormatOptions(0), metrics: nil, views: views)
-        var thumbnailConstrV =  NSLayoutConstraint.constraintsWithVisualFormat(imageVStr, options: NSLayoutFormatOptions(0), metrics: nil, views: views)
-        var titleConstrH = NSLayoutConstraint.constraintsWithVisualFormat(titleHStr, options: NSLayoutFormatOptions(0), metrics: nil, views: views)
-        var subtitleConstrH = NSLayoutConstraint.constraintsWithVisualFormat(subtitleHStr, options: NSLayoutFormatOptions(0), metrics: nil, views: views)
-        var labelConstrV = NSLayoutConstraint.constraintsWithVisualFormat(labelVStr, options: NSLayoutFormatOptions(0), metrics: nil, views: views)
-        
-        cell.addConstraints(thumbnailConstrH)
-        cell.addConstraints(thumbnailConstrV)
-        cell.addConstraints(titleConstrH)
-        cell.addConstraints(subtitleConstrH)
-        cell.addConstraints(labelConstrV)
-    }
 }
